f829d1986c58a8c0711d33f9df4fb9b4a0ad3993
Sahilgitlab <131391047+Sahilgitlab@users.noreply.github.com>
2023-04-20 22:37:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+window = screen
 window.addEventListener('load', () => {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
-window = screen
+
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
